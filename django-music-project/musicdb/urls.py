157a12f8fe42eba6deb68fee5c582ec772bc2e90
jayxrey <45931100+jayxrey@users.noreply.github.com>
2021-04-21 20:43:38 -0400

    fixed range  bug

--- django-music-project/musicdb/urls.py
+++ django-music-project/musicdb/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, re_path
 from musicapp import views
 from django.conf.urls import url
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/songs/$', views.songs_list),
-    re_path(r'^api/songs/([0-9])$', views.songs_detail),
+    re_path(r'^api/songs/(?P<pk>\d+)$', views.songs_detail),
 ]
